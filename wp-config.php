6d4ad53c8d9bda670d19bd556f30bb59bcffab0c
fgoodship <fgoodship@gmail.com>
2017-08-02 16:41:10 -0400

    added gitignore

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'd3wp');
+define('DB_NAME', 'd3-dev-3-db');
 
 /** MySQL database username */
 define('DB_USER', 'root');
