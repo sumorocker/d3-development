331145b9b8090fa39b519c7130386f03e11e7b62
fgoodship <fgoodship@gmail.com>
2017-08-08 11:47:10 -0400

    built categories section + acf

--- wp-content/themes/understrap/section-templates/categories-img-txt-section.php
+++ wp-content/themes/understrap/section-templates/categories-img-txt-section.php
@@ -3,22 +3,34 @@
  * @package understrap
  */
 ?>
-<div class="category-section">
+<div class="category-section" style="background-image:url('http://localhost:8888/d3-dev-3/wp-content/uploads/2017/08/Group-3.svg');">
 	<div class="row">
-	<div class="col-lg-1 offset-lg-5 shape-left"></div>
-		<div class="col-lg-6 no-padding">
-			<div class="img-w-category">
+
+	<div class="col-lg-1">
+	<h1 class="title">ZONES</h1>
+	</div>
+
+	<div class="col-lg-1 offset-lg-4 shape-left"></div>
+
+
+
+		<div class="col-lg-3">
+		<?php if(have_rows('categories_list')) : while(have_rows('categories_list')) : the_row(); ?>
+			<div class="img-w-category-left">
 				<ul>
-					<li class="category-item"><h2>category 1</h2></li>
-					<li class="category-item"><h2>category 2</h2></li>
-					<li class="category-item"><h2>category 3</h2></li>
-					<li class="category-item"><h2>category 4</h2></li>
-					<li class="category-item"><h2>category 5</h2></li>
-					<li class="category-item"><h2>category 6</h2></li>
-					<li class="category-item"><h2>category 7</h2></li>
-					<li class="category-item"><h2>category 8</h2></li>
+					<li class="category-item"><h2><?php the_sub_field('list_left_item'); ?></h2></li>
 				</ul>
 			</div>
+			<?php endwhile; endif; ?>
+		</div>
+		<div class="col-lg-3">
+			<?php if(have_rows('categories_list')) : while(have_rows('categories_list')) : the_row(); ?>
+			<div class="img-w-category-right">
+				<ul>
+					<li class="category-item"><h2><?php the_sub_field('list_right_item'); ?></h2></li>
+				</ul>
+			</div>
+			<?php endwhile; endif; ?>
 		</div>
 	</div>
 	<div class="row">
