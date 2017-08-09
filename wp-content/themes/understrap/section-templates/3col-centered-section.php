a50be6d16c7b93c2cf7eb44de1742f133bed8534
Allan MacDonald <macdonald.allan@live.com>
2017-08-09 18:17:16 -0400

    Revert "3-col-centered act fields"
    This reverts commit 8bca428ad64fbf80deced71a4bcb0e44230707a4.

--- wp-content/themes/understrap/section-templates/3col-centered-section.php
+++ wp-content/themes/understrap/section-templates/3col-centered-section.php
@@ -14,8 +14,8 @@
 		src="<?php the_field('background_img'); ?>" alt="">
 	<div class="row">
 		<div class="offset-lg-1 col-lg-6 three-col-title">
-			<h1><?php the_field('three_col_header'); ?></h1>
-			<p><?php the_field('three_col_subheader'); ?></p>
+			<h1>meet our coaches</h1>
+			<p>A seasoned entrepreneur you can count on to challenge you, hold you accountable, and help you avoid costly mistakes.</p>
 		</div>
 	</div>
   <div class='row justify-content-center'>
