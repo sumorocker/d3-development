b5a692b240980951b7f5ca9ff7238ecc18e2990a
fgoodship <fgoodship@gmail.com>
2017-08-24 14:03:14 -0400

    big merg

---
+++ wp-content/themes/understrap/loop-templates/content-startups.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Post rendering content according to caller of get_template_part.
+ *
+ * @package understrap
+ */
+
+?>
+
+
+<div class="col-lg-3 col-md-4 blog-single">
+<div class="startup-logo" style="background-image:url('<?php the_field('logo_img'); ?>')"></div>
+	<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+	<p><?php the_field('description'); ?></p>
+	<?php the_category( $separator, $parents, $post_id ); ?>
+	<a href="<?php the_field('link'); ?>"><button class="btn_orange"><header class="entry-header">
+
+			<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
+			'</a></h2>' ); ?>
+			<?php if ( 'post' == get_post_type() ) : ?>
+
+			<?php endif; ?>
+
+		</header></button></a><!-- .entry-header -->
+
+		<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+
+		<div class="entry-content">
+
+
+			<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+				'after'  => '</div>',
+			) );
+			?>
+			
+
+		</div><!-- .entry-content -->
+
+	</article><!-- #post-## -->
+</div>
--- wp-content/themes/understrap/section-templates/header-section.php
+++ wp-content/themes/understrap/section-templates/header-section.php
@@ -5,18 +5,19 @@
 ?>
 
 <div class="header-section">
-  <div class='row header-col-2' style="background-color:<?php the_field('background_color'); ?>">
-    <div class="header-text col-sm-6">
-      <h1><?php the_field('header_title'); ?></h1>
-      <h3><?php the_field('header_text'); ?></h3>
+  <div class='row header-col-2 justify-content-center' style="flex-direction:<?php the_field('image_align'); ?>;background-color:<?php the_field('background_color'); ?>">
+  <div class="lines"></div>
+    <div class="header-text col-sm-6" >
+      <h1 style="text-align:<?php the_field('header_text_align'); ?>;" ><?php the_field('header_title'); ?></h1>
+      <h3 style="text-align:<?php the_field('header_text_align'); ?>;" ><?php the_field('header_text'); ?></h3>
       <div class="header-section-button" style="display:<?php the_field('display_button'); ?>">
          <button style="display: <?php the_field('header_button_display'); ?>" type="button" class="<?php the_field('button_color'); ?>"><a href="<?php the_field('header_button_link'); ?>">
           <h2><?php the_field('header_button'); ?></h2></a>
         </button>
       </div>
     </div>
-    <div class="col-sm-6">
-        <div class="image-right">
+    <div class="col-sm-6" style="display:<?php the_field('image_display'); ?>">
+        <div class="image-right" >
           <img src="<?php the_field('header_img'); ?>" alt="">
         </div>
     </div>
