75821d9aa9c861fe33e09924a51d71708aa2bb4a
Allan MacDonald <macdonald.allan@live.com>
2017-08-24 16:23:46 -0400

    posts per page adjustments

--- wp-content/themes/understrap/inc/filtration.php
+++ wp-content/themes/understrap/inc/filtration.php
@@ -13,7 +13,7 @@ add_filter('excerpt_length', 'my_excerpt_length');
 add_filter('pre_get_posts', 'limit_archive_posts');
 function limit_archive_posts($query){
     if ($query->is_archive) {
-        $query->set('posts_per_page', 8);
+        $query->set('posts_per_page', 4);
     }
     return $query;
 }
